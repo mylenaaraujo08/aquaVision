c3edf93909e5343c675497ca58ea3da2988cf914
mylenaaraujo08 <mylena08@gmail.com>
2025-03-16 17:39:32 -0300

    Implementando a tela de cadastro com o consumo da api

--- project/pi-ionic/src/app/cadastro/cadastro.page.ts
+++ project/pi-ionic/src/app/cadastro/cadastro.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
 })
-export class CadastroPage implements OnInit {
+export class CadastroPage {
+  userData: any = {};
 
-  constructor() { }
+  constructor(private apiService: ApiService, private router: Router) { }
 
-  ngOnInit() {
+  register() {
+    if (this.userData.password !== this.userData.confirmPassword) {
+      console.log('Passwords do not match');
+      return;
+    }
+
+    this.apiService.createUser(this.userData).subscribe({
+      next: (response) => {
+        console.log('User registered successfully');
+        this.router.navigate(['/home']);
+      },
+      error: (error) => {
+        console.error('Error registering user', error);
+      }
+    });
   }
-
 }
