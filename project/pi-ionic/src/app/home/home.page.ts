21dab78e0c5c88467b78fb479ac052cbb14124a9
mylenaaraujo08 <mylena08@gmail.com>
2025-03-16 17:40:02 -0300

    Implementando a autenticaçao de usuário com consumo da api

--- project/pi-ionic/src/app/home/home.page.ts
+++ project/pi-ionic/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,23 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  sessionData: any = {};
 
   constructor(
-    private router: Router
+    private router: Router,
+    private apiService: ApiService
   ) {}
 
-  goToPage(a: string):void{
-    this.router.navigate([a]);
-  };
-
+  authenticateUser(): void {
+    this.apiService.authUser(this.sessionData).subscribe({
+      next: (response) => {
+        console.log('User authenticated successfully');
+        this.router.navigate(['/map']); // Navigate to the dashboard or any other page after successful authentication
+      },
+      error: (error) => {
+        console.error('Error authenticating user', error);
+        console.log(this.sessionData)
+      }
+    });
+  }
 }
