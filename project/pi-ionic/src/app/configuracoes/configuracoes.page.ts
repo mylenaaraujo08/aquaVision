d5300ef790da776b6f7dbe95c1c3312b9c1bc3eb
mylenaaraujo08 <mylena08@gmail.com>
2025-03-17 03:33:45 -0300

    Atualizando página de configurações

--- project/pi-ionic/src/app/configuracoes/configuracoes.page.ts
+++ project/pi-ionic/src/app/configuracoes/configuracoes.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { ModalSairPage } from '../modal-sair/modal-sair.page';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-configuracoes',
@@ -9,10 +10,12 @@ import { ModalSairPage } from '../modal-sair/modal-sair.page';
   styleUrls: ['./configuracoes.page.scss'],
 })
 export class ConfiguracoesPage implements OnInit {
-
+  userData: any;
+  
   constructor(
     private router: Router,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private user: AuthService
   ) {}
 
   async openModal(){
@@ -31,6 +34,7 @@ export class ConfiguracoesPage implements OnInit {
   
 
   ngOnInit() {
+    this.userData = this.user.getUserData();
   }
 
 }
